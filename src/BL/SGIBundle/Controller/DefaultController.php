b8de06def34fe8d9142521ec43d99e0f478d628d
root <mwveliz@gmail.com>
2016-04-18 23:45:01 -0430

    graphs with jsondata

--- src/BL/SGIBundle/Controller/DefaultController.php
+++ src/BL/SGIBundle/Controller/DefaultController.php
@@ -78,16 +78,48 @@ class DefaultController extends Controller
         $model=$request->get('model');
         $track_field=$request->get('track_field');
         
+        
+       // $campos = array('d.value', 'd.datetime', 'o.id');
+    //$fields = 'partial d.{id, name}, partial o.{id}';  //if you want to get entity object
+         $campos = array('t.value', 't.datetime');
+        $query = $em->createQueryBuilder();
+        $query
+            ->select($campos)
+            ->from('SGIBundle:TrackAltinv', 't')
+            //->leftjoin('d.otherEntity', 'o');
+            ->where('t.idFieldsTrackAltinv= :track_field')
+            ->setParameter('track_field', $track_field)
+            ->add('orderBy','t.datetime ASC');    
+        //$query->setMaxResults(10);
+        $registros= $query->getQuery()->getResult();
      
-        $fields=$em->getRepository('SGIBundle:TrackAltinv')
+       /* $fields=$em->getRepository('SGIBundle:TrackAltinv')
                 ->findBy(
                     array('idFieldsTrackAltinv'=> $track_field),
                     array('idFieldsTrackAltinv' => 'ASC')
             );
-                
-                
+         */       
+        
+
+
+        //cambios los indices para que pueda verse
+        foreach ( $registros as $k=>$v )
+        {
+            /*primero el eje de tiempo*/
+            $fecha=$registros[$k]['datetime']->format('Y-m-d H:i:s');
+            $registros[$k] ['date'] = substr($fecha,0,7);
+            
+            /**luego la(s) serie(s) de valores, debo calcular matriz*/
+            $valor=$registros[$k]['value'];
+            $registros[$k] ['serie1'] = floatval($valor);
+            
+            unset($registros[$k]['datetime']);
+            unset($registros[$k]['value']);
+        }
+        
+        
         $serializer = $this->container->get('serializer');
-        $objects= $serializer->serialize($fields, 'json');
+        $objects= $serializer->serialize($registros, 'json');
         
         
         return new Response($objects);
